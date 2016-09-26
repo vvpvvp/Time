e7970b484e41081a180e823f38c414d86318cf73
vvpvvp <alias8516@sina.com.cn>
2016-09-26 14:55:52 +0800

    暂存

--- www/js/index.js
+++ www/js/index.js
@@ -14,41 +14,111 @@ var app = {
         $(".app").show();
 
         FastClick.attach(document.body);
-        Op.refreshView();
+        MainPage.init();
+    }
+};
 
-        $(".app .empty.add").on("click", function() {
-            $(".create").addClass("show");
-            Op.getGeolocation();
-        });
-
-        $(".create .cancelCreate").on("click", function() {
-            $(".create").removeClass("show");
-        });
-
-        $(".locationSection .cancelCreate").on("click", function() {
-            $(".locationSection").removeClass("show");
-        });
-
-        $(".create .location").on("click", function() {
-            var l = Op.getLocations();
-            if (l) {
-                $(".locationSection").addClass("show");
-            } else {
-                // navigator.notification.alert("正在加载位置");
+var MainPage = {
+    content:null,
+    init:function(){
+        this.content = $(".template.app").clone();
+        this.content.removeClass("template")
+        $("body").append(this.content);
+        this.refreshView();
+        this.initEvents();
+    },
+    refreshView:function(){
+        var _this = this;
+        $(".list>div",_this.content).not(":first").remove();
+        Data.list(function(data){
+            var html = "";
+            for (var i = 0; i < data.length; i++) {
+                var d = data[i];
+                var m = moment(data[i].updateAt);
+                html += '<div><div class="calarder"><span class="date">'+m.date()+'</span>&nbsp;&nbsp;<span class="month">'+m.month()+'月</span></div>';
+                if(d.urls.length==0){
+                    html += '<div class="images empty"><div>空</div></div>';
+                }else{
+                    html += '<div class="images"><div class="img" style="background-image:url('+d.urls[0].src+')"></div></div>';
+                }
+                html += '<div class="content"><div class="word">'+d.content+'</div><div class="desc">'+d.address+'</div></div></div>';
             }
+            $(".list",_this.content).append(html);
+        });
+    },
+    initEvents(){
+        var _this = this;
+        $(".empty.add",_this.content).on("click", function() {
+            CreatePage.init();
+        });
+    }
+}
+
+
+var CreatePage = {
+    content:null,
+    init:function(){
+        var _this = this;
+        this.content = $(".template.create").clone();
+        this.content.removeClass("template");
+        $("body").append(this.content);
+        setTimeout(function() {
+            _this.content.addClass("show");
+        }, 0);
+        this.getGeolocation();
+        this.initEvents();
+    },
+    destroy(){
+        var _this = this;
+        this.content.removeClass("show");
+        setTimeout(function() {
+            _this.content.remove();
+        }, 500);
+    },
+    getGeolocation:function() {
+        D.geo = {};
+        var _this = this;
+        $(".location-desc",_this.content).text("正在加载地址");
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var coords = D.coords = GPS.gcj_encrypt(position.coords.latitude,position.coords.longitude);
+            // var coords = D.coords = GPS.gcj_encrypt(100.015907,9.517026);
+            position.location = (coords.lon) + "," + (coords.lat);
+            position.googleLocation = (coords.lat) + "," + (coords.lon);
+            D.geo = position;
+            // console.log(position.googleLocation);
+            $(".location-desc",_this.content).text("已获取坐标");
+            Address.getAddress(function(l){
+                D.geo.address = l;
+                $(".location-desc",_this.content).text(l);
+            });
+        }, function(error) {
+        }, { maximumAge: 3000, timeout: 100000, enableHighAccuracy: true });
+    },
+    initEvents(){
+        var _this = this;
+        
+        $(".cancelCreate",_this.content).on("click", function() {
+            _this.destroy();
+        });
+
+        $(".location",_this.content).on("click", function() {
+            if (!D.geo.location) {
+                return false;
+            }
+            locationSectionPage.init();
 
         });
 
-        $(".create #send").on("click",function(){
+        $("#send",_this.content).on("click",function(){
             var srcs = [],param = {};
-            $(".create .img-picker [src]").each(function(i,n) {
+            $(".img-picker [src]",_this.content).each(function(i,n) {
                 // console.log($(n).attr("src"));
                 var _n = $(n);
                 srcs.push({src:_n.attr("src"),type:_n.attr("v-type"),isPosed:false});
             })
-            var content = $(".create textarea").val();
+            var content = $("textarea",_this.content).val();
             if(srcs.length==0&&content==""){
-                $(".create").removeClass("show");
+                _this.destroy();
             }
             var time = (new Date()).getTime();
             var param = {
@@ -61,15 +131,15 @@ var app = {
                 isPosed:0
             };
             Data.save(param,function(){
-                Op.refreshView();
-                $(".create").removeClass("show");
+                MainPage.refreshView();
+                _this.destroy();
             });
             // Op.refreshApp();
             Request.postData(param);
         });
 
 
-        $(".create .takePicture").on("click", function() {
+        $(".takePicture",_this.content).on("click", function() {
             menuControl([
                 {
                     op:"拍照",
@@ -89,6 +159,59 @@ var app = {
                     }
                 }
             ]);
+        });
+    }
+};
+
+var locationSectionPage = {
+    content:null,
+    init:function(){
+        var _this = this;
+        this.content = $(".template.locationSection").clone();
+        this.content.removeClass("template");
+        $("body").append(this.content);
+        setTimeout(function() {
+            _this.content.addClass("show");
+        }, 0);
+        this.getLocations();
+        this.initEvents();
+    },
+    destroy(){
+        var _this = this;
+        this.content.removeClass("show");
+        setTimeout(function() {
+            _this.content.remove();
+        }, 500);
+    },
+    getLocations:function() {
+        var _this = this;
+        $(".listloadding",_this.content).show();
+        $(".locationList",_this.content).empty();
+
+        Address.getPOI(function(locations){
+            var html = "";
+            for(var i = 0; i < locations.length; i++){
+                var loc = locations[i];
+                html += '<li>';
+                html += '<p class="word">'+loc.title+'</p>';
+                if(loc.description)html += '<p class="font12 gray1">'+loc.description+'</p>';
+                html += '</li>';
+            }
+            $(".locationList",_this.content).append(html);
+            $(".listloadding",_this.content).hide();
+            $(".locationList li",_this.content).on("click",function(){
+                var l = $(".word",this).text();
+                D.geo.address = l;
+                $(".location-desc",CreatePage.content).text(l);
+                _this.destroy();
+            });
+        });
+        return true;
+    },
+    initEvents(){
+        var _this = this;
+        $(".cancelCreate",_this.content).on("click", function() {
+            _this.destroy();
         });
     }
 };
--- www/js/op.js
+++ www/js/op.js
@@ -2,7 +2,7 @@
 var Op = {
     captureAudio:function(){
         navigator.device.capture.captureAudio(function(imageURI) {
-            $(".create .takePicture").before('<div class="images"><div class="audio icon-mic" v-type="audio" src="'+imageURI[0].fullPath+'"></div></div>');
+            $(".takePicture",CreatePage.content).before('<div class="images"><div class="audio icon-mic" v-type="audio" src="'+imageURI[0].fullPath+'"></div></div>');
         }, function(){}, {limit: 2});
     },
     getPicture: function() {
@@ -17,7 +17,7 @@ var Op = {
         // );
         navigator.camera.getPicture(function(imageURI) {
             var src = imageURI;
-            $(".create .takePicture").before('<div class="images"><div class="img" v-type="img" style="background-image:url(' + src + ')" src="'+src+'"></div></div>');
+            $(".takePicture",CreatePage.content).before('<div class="images"><div class="img" v-type="img" style="background-image:url(' + src + ')" src="'+src+'"></div></div>');
         }, function(message) {
             // navigator.notification.alert("拍照失败，原因：" + message, null, "警告");
         }, {
@@ -34,7 +34,7 @@ var Op = {
     takePicture: function() {
         navigator.camera.getPicture(function(imageURI) {
             var src = imageURI;
-            $(".create .takePicture").before('<div class="images"><div class="img" v-type="img" style="background-image:url(' + src + ')" src="'+src+'"></div></div>');
+            $(".takePicture",CreatePage.content).before('<div class="images"><div class="img" v-type="img" style="background-image:url(' + src + ')" src="'+src+'"></div></div>');
         }, function(message) {
             // navigator.notification.alert("拍照失败，原因：" + message, null, "警告");
         }, {
@@ -46,70 +46,6 @@ var Op = {
             quality: 50,
             targetWidth:768, //图片输出宽度
             targetHeight:1280 //图片输出高度
-        });
-    },
-    getLocations:function() {
-        if (!D.geo.location) {
-            return false;
-        }
-        $(".listloadding").show();
-        $(".locationList").empty();
-
-        Address.getPOI(function(locations){
-            var html = "";
-            for(var i = 0; i < locations.length; i++){
-                var loc = locations[i];
-                html += '<li>';
-                html += '<p class="word">'+loc.title+'</p>';
-                if(loc.description)html += '<p class="font12 gray1">'+loc.description+'</p>';
-                html += '</li>';
-            }
-            $(".locationList").append(html);
-            $(".listloadding").hide();
-            $(".locationList li").on("click",function(){
-                var l = $(".word",this).text();
-                D.geo.address = l;
-                $(".location-desc").text(l);
-                $(".locationSection").removeClass("show");
-            });
-        });
-        return true;
-    },
-    getGeolocation:function() {
-        D.geo = {};
-        $(".location-desc").text("正在加载地址");
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var coords = D.coords = GPS.gcj_encrypt(position.coords.latitude,position.coords.longitude);
-            // var coords = D.coords = GPS.gcj_encrypt(100.015907,9.517026);
-            position.location = (coords.lon) + "," + (coords.lat);
-            position.googleLocation = (coords.lat) + "," + (coords.lon);
-            D.geo = position;
-            // console.log(position.googleLocation);
-            $(".location-desc").text("已获取坐标");
-            Address.getAddress(function(l){
-                D.geo.address = l;
-                $(".location-desc").text(l);
-            });
-        }, function(error) {
-        }, { maximumAge: 3000, timeout: 100000, enableHighAccuracy: true });
-    },
-    refreshView:function(){
-        $(".app .list>div").not(":first").remove();
-
-        Data.list(function(data){
-            var html = "";
-            for (var i = 0; i < data.length; i++) {
-                var d = data[i];
-                var m = moment(data[i].updateAt);
-                html += '<div><div class="calarder"><span class="date">'+m.date()+'</span>&nbsp;&nbsp;<span class="month">'+m.month()+'月</span></div>';
-                if(d.urls.length==0){
-                    html += '<div class="images empty"><div>空</div></div>';
-                }else{
-                    html += '<div class="images"><div class="img" style="background-image:url('+d.urls[0].src+')"></div></div>';
-                }
-                html += '<div class="content"><div class="word">'+d.content+'</div><div class="desc">'+d.address+'</div></div></div>';
-            }
-            $(".app .list").append(html);
         });
     }
 };
